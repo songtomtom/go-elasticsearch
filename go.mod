d2eecd2a0a7c2fb5fad684cdfec5caa7632249f1
songtomtom <tom.song@luxrobo.com>
2023-05-25 19:20:00 +0900

    펄슨 테스트

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module songtomtom/go-elasticsearch
 
 go 1.19
 
-require github.com/elastic/go-elasticsearch/v7 v7.17.10 // indirect
+require github.com/elastic/go-elasticsearch/v7 v7.17.10
--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
-	"github.com/elastic/go-elasticsearch/v7"
-	"github.com/elastic/go-elasticsearch/v7/esapi"
 	"log"
 	"strings"
+
+	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
 )
 
+type Person struct {
+	Name   string `json:"name"`
+	Age    int    `json:"age"`
+	Gender string `json:"gender"`
+}
+
 func main() {
-	// 1. Elasticsearch 클라이언트 설정
 	cfg := elasticsearch.Config{
 		Addresses: []string{
 			"http://localhost:9200",
@@ -22,78 +27,33 @@ func main() {
 		log.Fatalf("Error creating the client: %s", err)
 	}
 
-	// 2. 새로운 도큐먼트 색인
-	var b strings.Builder
-	b.WriteString(`{"title" : "Test", "content" : "Elasticsearch 사용 테스트"}`)
-	req := esapi.IndexRequest{
-		Index:   "test-index",
-		Body:    strings.NewReader(b.String()),
-		Refresh: "true",
-	}
-	res, err := req.Do(context.Background(), es)
-	if err != nil {
-		log.Fatalf("Error getting response: %s", err)
-	}
-	defer res.Body.Close()
-
-	// 3. 도큐먼트 검색
-	var buf bytes.Buffer
-	query := map[string]interface{}{
-		"query": map[string]interface{}{
-			"match": map[string]interface{}{
-				"title": "Test",
-			},
-		},
-	}
-	if err := json.NewEncoder(&buf).Encode(query); err != nil {
-		log.Fatalf("Error encoding query: %s", err)
+	people := []Person{
+		{Name: "John Doe", Age: 30, Gender: "Male"},
+		{Name: "Jane Doe", Age: 28, Gender: "Female"},
 	}
 
-	res, err = es.Search(
-		es.Search.WithContext(context.Background()),
-		es.Search.WithIndex("test-index"),
-		es.Search.WithBody(&buf),
-		es.Search.WithTrackTotalHits(true),
-		es.Search.WithPretty(),
-	)
-	if err != nil {
-		log.Fatalf("Error getting response: %s", err)
-	}
-	defer res.Body.Close()
-
-	if res.IsError() {
-		var e map[string]interface{}
-		if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
-			log.Fatalf("Error parsing the response body: %s", err)
-		} else {
-			// Print the response status and error information.
-			log.Fatalf(
-				"[%s] %s: %s",
-				res.Status(),
-				e["error"].(map[string]interface{})["type"],
-				e["error"].(map[string]interface{})["reason"],
-			)
+	for _, person := range people {
+		personJson, err := json.Marshal(person)
+		if err != nil {
+			log.Fatalf("Error marshaling the person: %s", err)
 		}
-	}
 
-	var r map[string]interface{}
-	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
-		log.Fatalf("Error parsing the response body: %s", err)
-	}
+		req := esapi.IndexRequest{
+			Index:      "test-index",
+			DocumentID: person.Name,
+			Body:       strings.NewReader(string(personJson)),
+		}
 
-	// Print the response status, number of results, and request duration.
-	log.Printf(
-		"[%s] %d hits; took: %dms",
-		res.Status(),
-		int(r["hits"].(map[string]interface{})["total"].(map[string]interface{})["value"].(float64)),
-		int(r["took"].(float64)),
-	)
-	// Print each hit.
-	for _, hit := range r["hits"].(map[string]interface{})["hits"].([]interface{}) {
-		log.Printf(
-			" * id=%s, index=%s",
-			hit.(map[string]interface{})["_id"],
-			hit.(map[string]interface{})["_index"],
-		)
+		res, err := req.Do(context.Background(), es)
+		if err != nil {
+			log.Fatalf("Error getting response: %s", err)
+		}
+		defer res.Body.Close()
+
+		if res.IsError() {
+			log.Println(res.String())
+		} else {
+			log.Printf("Successfully indexed document: %s", person.Name)
+		}
 	}
 }
